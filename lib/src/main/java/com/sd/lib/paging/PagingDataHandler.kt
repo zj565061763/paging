0c36fc415574e29ed1309a13199cb2bba3c24a10
zjun <565061763@qq.com>
2025-01-04 01:10:41 +0800

    Comment

--- lib/src/main/java/com/sd/lib/paging/PagingDataHandler.kt
+++ lib/src/main/java/com/sd/lib/paging/PagingDataHandler.kt
@@ -5,7 +5,8 @@ import kotlinx.coroutines.withContext
 
 abstract class PagingDataHandler<Key : Any, Value : Any> {
   /**
-   * 处理第每页的数据，并返回总数据，如果返回null表示不处理，[handlePageData]不会并发
+   * 处理第每页的数据，并返回总数据，如果返回null表示不处理，
+   * [handlePageData]在主线程触发
    */
   abstract suspend fun handlePageData(
     params: LoadParams<Key>,
