cd810095eecc702ea77d25ac3dab4cfae610db70
zjun <565061763@qq.com>
2025-01-03 17:33:16 +0800

    Comment

--- lib/src/main/java/com/sd/lib/paging/LoadState.kt
+++ lib/src/main/java/com/sd/lib/paging/LoadState.kt
@@ -2,16 +2,21 @@ package com.sd.lib.paging
 
 sealed interface LoadState {
   data class NotLoading(
+    /** 是否已到达末页 */
     val endOfPaginationReached: Boolean,
   ) : LoadState {
     companion object {
+      /** 未到达末页 */
       val Incomplete = NotLoading(endOfPaginationReached = false)
+      /** 已到达末页 */
       val Complete = NotLoading(endOfPaginationReached = true)
     }
   }
 
+  /** 加载中 */
   data object Loading : LoadState
 
+  /** 加载失败 */
   data class Error(
     val error: Throwable,
   ) : LoadState
