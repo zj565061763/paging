bfd3aeff59ba9e0c566c0898220fe2a842e05e1c
zjun <565061763@qq.com>
2025-01-07 00:20:41 +0800

    AppendIfLastItem逻辑优化

--- lib/src/main/java/com/sd/lib/paging/compose/LazyExt.kt
+++ lib/src/main/java/com/sd/lib/paging/compose/LazyExt.kt
@@ -2,12 +2,23 @@ package com.sd.lib.paging.compose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import com.sd.lib.paging.LoadState
 
 @Composable
 internal fun <T : Any> AppendIfLastItem(paging: PagingPresenter<T>, item: T) {
   if (item !== paging.items.lastOrNull()) return
-  if (paging.refreshLoadState == LoadState.NotLoading.Complete) {
+
+  val isRefreshReady by remember(paging) {
+    derivedStateOf {
+      paging.refreshLoadState == LoadState.NotLoading.Complete
+        || paging.refreshLoadState is LoadState.Error
+    }
+  }
+
+  if (isRefreshReady) {
     LaunchedEffect(paging) {
       if (paging.appendLoadState == LoadState.NotLoading.Incomplete) {
         paging.append()
