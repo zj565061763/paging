b0506cc7aae60fc16d42b28ea5d90db8209e80ca
zjun <565061763@qq.com>
2025-01-07 23:46:43 +0800

    1.0.0-alpha04

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "paging"
-val libVersion = "1.0.0-alpha03"
+val libVersion = "1.0.0-alpha04"
 
 android {
   namespace = "com.sd.lib.paging"
